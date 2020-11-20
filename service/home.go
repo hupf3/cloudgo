dc773a70f02dc7f41bd299c082e19ae2830cfd52
hupf <945554668@qq.com>
2020-11-20 15:21:47 +0800

    更新登陆界面

--- service/home.go
+++ service/home.go
@@ -9,7 +9,7 @@ import (
 func homeHandler(formatter *render.Render) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.HTML(w, http.StatusOK, "index", struct {
+		formatter.HTML(w, http.StatusOK, "login", struct {
 			ID      string `json:"id"`
 			Content string `json:"content"`
 		}{ID: "8675309", Content: "Hello from Go!"})
