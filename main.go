ec520a54f0ecaba803e03e6b6b6b848f79f396ee
hupf <945554668@qq.com>
2020-11-20 12:54:05 +0800

    init

--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strings"
+	"os"
+
+	"gitee.com/zzm99/cloudgo/service"
+	flag "github.com/spf13/pflag"
+)
+
+const (
+	PORT string = "8080"
 )
 
 func main() {
-	err := http.ListenAndServe(":9090", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm() //解析参数，默认是不会解析的
-		//fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-		fmt.Println("path", r.URL.Path)
-		fmt.Println("scheme", r.URL.Scheme)
-		fmt.Println(r.Form["url_long"])
-		for k, v := range r.Form {
-			fmt.Println("key:", k)
-			fmt.Println("val:", strings.Join(v, ""))
-		}
-		fmt.Fprintf(w, "Hello boys and girls! \n") //这个写入到 w 的是输出到客户端的
-	})) //设置监听的端口
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = PORT
 	}
+
+	pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
+	flag.Parse()
+
+	if len(*pPort) != 0 {
+		port = *pPort
+	}
+
+	server := service.NewServer()
+	server.Run(":" + port)
 }
